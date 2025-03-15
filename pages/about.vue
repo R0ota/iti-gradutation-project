d712e1ac99d765a90983a9b391496056db4125b5
reham muhammed <reham.mohamed.ma@gmail.com>
2025-03-15 09:56:16 +0200

    initialize Nuxt.js application with Tailwind CSS and basic structure

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900 p-6"
+  >
+    <h2 class="text-3xl font-bold mb-4">Home Page</h2>
+    <p class="text-lg text-red-700">Welcome to our about.</p>
+  </div>
+</template>
