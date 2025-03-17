d23e01ed96a39781949bda01adddfa1234145f47
Norhan Ibrahim <norhanibrahim289@gmail.com>
2025-03-17 22:25:10 +0200

    product-card

--- Front End/pages/market.vue
+++ Front End/pages/market.vue
@@ -10,18 +10,18 @@ const {data} = await useAsyncFetch("get", "/products")
 
 <template>
 
-  <div class="lg:ml-[135px] lg:mr-[135px] lg:gap-[32px] flex flex-col gap-[24px] ml-[61px] mr-[61px] mt-[32px]">
+  <div class="lg:ml-[135px] lg:mr-[135px] lg:gap-[32px] flex flex-col gap-[24px] ml-[61px] mr-[61px] ">
     <div class="text-[20px] tracking-[-0.38px] float-left font-[Poppins] lg:text-[24px] font-semibold leading-[150%] lg:tracking-[-0.456px]">
       Market Catigories
     </div>
-    <div class="flex flex-row justify-between flex-wrap ">
+    <div class="flex flex-row justify-center gap-4 flex-wrap ">
       <div  v-for="item in data.products">
         <CardType  >{{ item.title }}</CardType>
       </div>
     </div> 
     <div class="flex flex-col items-center">
-      <div class=" lg:w-[640px] lg:stroke-[2px] stroke-black">
-        <svg  xmlns="http://www.w3.org/2000/svg" width="642" height="2" viewBox="0 0 642 2" fill="none">
+      <div class=" lg:w-[640px] lg:stroke-[2px] stroke-black w-[370px] stroke-[1px]">
+        <svg  xmlns="http://www.w3.org/2000/svg" width-full height-auto viewBox="0 0 642 2" fill="none">
         <path d="M641 1L1 1" stroke="black" stroke-width="2" stroke-linecap="round"/>
         </svg>
       </div>
@@ -32,8 +32,14 @@ const {data} = await useAsyncFetch("get", "/products")
       </p>
       <p class="lg:text-[16px] tracking-[-0.304px] font-semibold cursor-pointer">more</p>
     </div>
+    <div  class="flex flex-row  lg:flex-wrap lg:justify-between  overflow-x-auto gap-4">
+      <div v-for="product in data.products" class="flex">
+      <ProductCard :name = "product.title" :type = "product.title" :price = "product.id"/>
       
-    <ProductCard>{{ item.title }} </ProductCard>
+    </div>
+    </div>
+    
+    
     
         
   </div>
