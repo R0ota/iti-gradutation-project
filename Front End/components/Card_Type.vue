dc005822808b34e9efbb652093b919b274f55fd7
Norhan Ibrahim <norhanibrahim289@gmail.com>
2025-03-17 17:42:46 +0200

    Edit in Card-Type

--- Front End/components/Card_Type.vue
+++ Front End/components/Card_Type.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="flex flex-col justify-end items-center 
-    gap-2.5 w-[341px] h-[480px] p-[24px_27px] mt-[32px]
-    rounded-[40px] bg-[#D9D9D9] sm:w-[247.935px] 
-    sm:h-[349px] sm:p-[17.45px_19.631px] sm:gap-[7.271px] sm:mt-[24px]">
-
+    <div class=" mobile-style flex flex-col justify-end items-center 
+    lg:gap-2.5 lg:w-[341px] lg:h-[480px] lg:p-[24px_27px] lg:mt-[4px] lg:mb-[24px]
+    rounded-[40px] bg-[#D9D9D9] w-[247.935px] 
+    h-[349px] p-[17.45px_19.631px] mt-[8px] mb-[16px]">
+    <slot />
     <button class="flex justify-center items-center 
-    gap-[10px] p-[8px_24px] rounded-[16px] 
-  bg-[#FFF] cursor-pointer sm:p-[5.817px_17.45px] 
-    md:p-[8px_22px] sm:rounded-[11.633px] 
-    text-[16px] font-semibold leading-[150%] tracking-[-0.304px] sm:tracking-[-0.276px] sm:text-[14.542px]">Product Type</button>
+    lg:gap-[10px] lg:p-[8px_24px] lg:rounded-[16px] 
+  bg-[#FFF] cursor-pointer p-[5.817px_17.45px] 
+    md:p-[8px_22px] rounded-[11.633px] 
+    lg:text-[16px] lg:font-semibold leading-[150%] lg:tracking-[-0.304px] tracking-[-0.276px] text-[14.542px]">Product Type</button>
     </div>
 </template>
--- Front End/pages/market.vue
+++ Front End/pages/market.vue
@@ -3,10 +3,40 @@ definePageMeta({
   layout: "market",
 });
 
+const {data} = await useAsyncFetch("get", "/products")
+  console.log(data);
 
 </script>
 
 <template>
-  <div class=" font-[Poppins] text-[24px] font-semibold leading-[150%] tracking-[-0.456px]">Market Catigories</div>
+
+  <div class="lg:ml-[135px] lg:mr-[135px] lg:gap-[32px] flex flex-col gap-[24px] ml-[61px] mr-[61px] mt-[32px]">
+    <div class="text-[20px] tracking-[-0.38px] float-left font-[Poppins] lg:text-[24px] font-semibold leading-[150%] lg:tracking-[-0.456px]">
+      Market Catigories
+    </div>
+    <div class="flex flex-row justify-between flex-wrap ">
+      <div  v-for="item in data.products">
+        <CardType  >{{ item.title }}</CardType>
+      </div>
+    </div> 
+    <div class="flex flex-col items-center">
+      <div class=" lg:w-[640px] lg:stroke-[2px] stroke-black">
+        <svg  xmlns="http://www.w3.org/2000/svg" width="642" height="2" viewBox="0 0 642 2" fill="none">
+        <path d="M641 1L1 1" stroke="black" stroke-width="2" stroke-linecap="round"/>
+        </svg>
+      </div>
+    </div>
+    <div class="flex flex-row justify-between ">
+      <p class="text-[20px] tracking-[-0.38px]  font-[Poppins] lg:text-[24px] font-semibold leading-[150%] lg:tracking-[-0.456px]">
+        Best-selling products
+      </p>
+      <p class="lg:text-[16px] tracking-[-0.304px] font-semibold cursor-pointer">more</p>
+    </div>
+      
+    <ProductCard>{{ item.title }} </ProductCard>
+    
+        
+  </div>
+  
   
 </template>
