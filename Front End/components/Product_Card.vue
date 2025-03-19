58c32f02eb3e3eeb14128a85f5bedc0be501d8c4
Norhan Ibrahim <norhanibrahim289@gmail.com>
2025-03-19 11:26:48 +0200

    edit hover

--- Front End/components/Product_Card.vue
+++ Front End/components/Product_Card.vue
@@ -5,38 +5,61 @@
   price:Number,
   image : String,
 });
+
+import { ref , nextTick} from "vue";
+
+const isFavorite = ref(false);
+
+const toggleFavorite = () => {
+  isFavorite.value = !isFavorite.value;
+  console.log("Favorite status:", isFavorite.value);
+};
 </script>
 
 <template>
     <div class="flex flex-col items-center lg:gap-[8px] gap-[5.039px] ">
-        <div class="relative lg:w-[254px] lg:h-[254px] lg:rounded-[40px] cursor-pointer bg-[#D9D9D9] flex-shrink-0 aspect-square w-[160px] h-[160px] rounded-[25.197px]">
-            <svg class="absolute top-4 right-4 " xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
-            <g filter="url(#filter0_d_477_6191)" >
-            <rect x="4" y="4" width="32" height="32" rx="16" fill="white"/>
-            <path d="M18.8203 28.5758L18.7227 28.486L11.8789 22.1305C10.6797 21.0172 10 19.4547 10 17.818V17.6891C10 14.9391 11.9531 12.5797 14.6562 12.0641C16.1953 11.7672 17.7695 12.1227 19.0234 13.0055C19.375 13.2555 19.7031 13.5446 20 13.8766C20.1641 13.6891 20.3398 13.5172 20.5273 13.3571C20.6719 13.2321 20.8203 13.1149 20.9766 13.0055C22.2305 12.1227 23.8047 11.7672 25.3438 12.0602C28.0469 12.5758 30 14.9391 30 17.6891V17.818C30 19.4547 29.3203 21.0172 28.1211 22.1305L21.2773 28.486L21.1797 28.5758C20.8594 28.8727 20.4375 29.0407 20 29.0407C19.5625 29.0407 19.1406 28.8766 18.8203 28.5758ZM19.3398 15.9508C19.3242 15.9391 19.3125 15.9235 19.3008 15.9078L18.6055 15.1266L18.6016 15.1227C17.6992 14.111 16.3359 13.65 15.0078 13.9039C13.1875 14.2516 11.875 15.8375 11.875 17.6891V17.818C11.875 18.9313 12.3398 19.9977 13.1562 20.7555L20 27.111L26.8438 20.7555C27.6602 19.9977 28.125 18.9313 28.125 17.818V17.6891C28.125 15.8414 26.8125 14.2516 24.9961 13.9039C23.668 13.65 22.3008 14.1149 21.4023 15.1227C21.4023 15.1227 21.4023 15.1227 21.3984 15.1266C21.3945 15.1305 21.3984 15.1266 21.3945 15.1305L20.6992 15.9118C20.6875 15.9274 20.6719 15.9391 20.6602 15.9547C20.4844 16.1305 20.2461 16.2282 20 16.2282C19.7539 16.2282 19.5156 16.1305 19.3398 15.9547V15.9508Z" fill="black"/>
-            </g>
-            <defs>
-            <filter id="filter0_d_477_6191" x="0" y="0" width="40" height="40" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-            <feOffset/>
-            <feGaussianBlur stdDeviation="2"/>
-            <feComposite in2="hardAlpha" operator="out"/>
-            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"/>
-            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_477_6191"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_477_6191" result="shape"/>
-            </filter>
-            </defs>
+        <div class="relative group lg:w-[254px] lg:h-[254px] lg:rounded-[40px] bg-[#D9D9D9] flex-shrink-0 aspect-square 
+        w-[160px] h-[160px] rounded-[25.197px]">
+
+            <svg  v-if="isFavorite"
+                @click="toggleFavorite" 
+                xmlns="http://www.w3.org/2000/svg" class="z-10 absolute lg:top-3 pointer-events-auto lg:right-3 top-2 right-2 
+                cursor-pointer transition-all duration-300" width="33" height="32" viewBox="0 0 33 32" fill="none">
+                <rect x="0.333008" width="32" height="32" rx="16" fill="white" class="transition-all duration-300 group-hover:fill-[#ECDCBF]"/>
+                <path d="M7.44826 19.0098L15.152 26.202C15.4717 26.5004 15.8938 26.6667 16.3329 26.6667C16.772 26.6667 17.1941 26.5004 
+                17.5138 26.202L25.2175 19.0098C26.5136 17.8033 27.2469 16.1108 27.2469 14.3416V14.0943C27.2469 11.1143 25.0939 8.57337 
+                22.1565 8.0831C20.2125 7.75909 18.2343 8.39431 16.8445 9.78414L16.3329 10.2957L15.8213 9.78414C14.4315 8.39431 12.4533 
+                7.75909 10.5093 8.0831C7.57189 8.57337 5.41895 11.1143 5.41895 14.0943V14.3416C5.41895 16.1108 6.15223 17.8033 7.44826 
+                19.0098Z" fill="#A31D1D"/>
             </svg>
 
-            <img :src="image" class="lg:w-[254px] lg:h-[254px] lg:rounded-[40px] w-[160px] h-[160px] rounded-[25.197px] hover:opacity-[0.5]">
+            <svg  v-else
+                @click="toggleFavorite" 
+                xmlns="http://www.w3.org/2000/svg" class="z-10 absolute pointer-events-auto lg:top-3 lg:right-3 top-2 right-2 
+                cursor-pointer transition-all duration-300 " width="32" height="32" viewBox="0 0 32 32" fill="none">
+                <rect width="32" height="32" rx="16" fill="white" class="transition-all duration-300 group-hover:fill-[#ECDCBF]"/>
+                <path d="M14.7113 26.1587L14.6046 26.0605L7.12557 19.1151C5.81504 17.8985 5.07227 16.191 5.07227 
+                14.4023V14.2614C5.07227 11.2562 7.20668 8.67782 10.1607 8.11433C11.8426 7.7899 13.563 8.17837 14.9333 
+                9.14312C15.3175 9.41633 15.676 9.73222 16.0005 10.0951C16.1798 9.89017 16.3719 9.70234 16.5768 9.52732C16.7347 
+                9.39071 16.8969 9.26265 17.0677 9.14312C18.438 8.17837 20.1583 7.7899 21.8402 8.11006C24.7943 8.67355 26.9287 
+                11.2562 26.9287 14.2614V14.4023C26.9287 16.191 26.1859 17.8985 24.8754 19.1151L17.3964 26.0605L17.2897 26.1587C16.9396 
+                26.4831 16.4786 26.6667 16.0005 26.6667C15.5224 26.6667 15.0613 26.4874 14.7113 26.1587ZM15.279 12.3618C15.262 12.349 
+                15.2492 12.3319 15.2363 12.3149L14.4765 11.4611L14.4722 11.4568C13.4861 10.3512 11.9963 9.84748 10.5449 10.125C8.55563 
+                10.5049 7.1213 12.238 7.1213 14.2614V14.4023C7.1213 15.6189 7.62929 16.7843 8.52148 17.6125L16.0005 24.5579L23.4795 
+                17.6125C24.3716 16.7843 24.8796 15.6189 24.8796 14.4023V14.2614C24.8796 12.2423 23.4453 10.5049 21.4603 10.125C20.0089 
+                9.84748 18.5148 10.3555 17.533 11.4568C17.533 11.4568 17.533 11.4568 17.5287 11.4611C17.5244 11.4654 17.5287 11.4611 
+                17.5244 11.4654L16.7646 12.3191C16.7518 12.3362 16.7347 12.349 16.7219 12.3661C16.5298 12.5582 16.2694 12.6649 16.0005 
+                12.6649C15.7315 12.6649 15.4711 12.5582 15.279 12.3661V12.3618Z" fill="#A31D1D"/>
+            </svg>
+
+            <img :src="image" class="lg:w-[254px] lg:h-[254px] lg:rounded-[40px] w-[160px] h-[160px] rounded-[25.197px] ">
         </div>
         
         <div class="flex flex-col items-start lg:gap-[2px] self-stretch">
-            <p class="text-right font-poppins lg:text-[18px] lg:tracking-[-0.342px] font-bold leading-[150%] text-[11.339px] tracking-[-0.215px]">
+            <p class="text-right font-poppins lg:text-[18px] lg:tracking-[-0.342px] font-bold leading-[150%] text-[11.339px] tracking-[-0.215px] text-red-800">
                 {{ name }}
             </p>
-            <p class="text-right font-poppins lg:text-[14px] text-[10px] font-medium leading-[150%] lg:tracking-[-0.266px] tracking-[-0.191] text-[#646464] ">
+            <p class="text-right font-poppins lg:text-[14px] text-[10px] text-black font-medium leading-[150%] lg:tracking-[-0.266px] tracking-[-0.191]">
                 {{ type }}
             </p>
             <p class="text-right font-poppins lg:text-[18px] lg:tracking-[-0.342px] font-bold leading-[150%] text-[11.339px] tracking-[-0.215px]">
--- Front End/pages/market.vue
+++ Front End/pages/market.vue
@@ -5,7 +5,6 @@ definePageMeta({
 
 const {data} = await useAsyncFetch("get", "/products")
   console.log(data);
-
 </script>
 
 <template>
@@ -21,11 +20,25 @@ const {data} = await useAsyncFetch("get", "/products")
       </div>
     </div> 
 
-    <div class="flex flex-row justify-between ">
-      <p class="text-[20px] tracking-[-0.38px] lg:text-[24px] font-semibold leading-[150%] lg:tracking-[-0.456px]">
-        Best-selling products
-      </p>
-      <p class="lg:text-[16px] tracking-[-0.304px] font-semibold cursor-pointer">more</p>
+    <div class="flex flex-row justify-between lg:mt-4 m-2">
+      <div class="px-6 border-l-[6px] border-red-800 inline-flex items-center">
+        <p class="justify-start text-red-800 lg:text-3xl text-xl font-bold  leading-loose lg:leading-[48px]">
+          Best-selling products
+        </p>
+      </div>
+      <div class="flex flex-row items-center gap-1">
+        <p class="lg:text-[16px] tracking-[-0.304px] font-semibold cursor-pointer">more</p>
+        <svg class="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <path d="M17.3261 8.74989C17.7871 9.21087 17.7871 9.95949 17.3261 10.4205L11.4257 16.3209C10.9647 
+          16.7819 10.2161 16.7819 9.7551 16.3209C9.29413 15.86 9.29413 15.1113 9.7551 14.6504L14.8221 9.58333L9.75879 
+          4.5163C9.29781 4.05533 9.29781 3.30671 9.75879 2.84573C10.2198 2.38476 10.9684 2.38476 11.4294 2.84573L17.3298 
+          8.7462L17.3261 8.74989ZM4.34511 2.84942L10.2456 8.74989C10.7066 9.21087 10.7066 9.95949 10.2456 10.4205L4.34511 
+          16.3209C3.88413 16.7819 3.13551 16.7819 2.67453 16.3209C2.21356 15.86 2.21356 15.1113 2.67453 14.6504L7.74156 
+          9.58333L2.67822 4.5163C2.21725 4.05533 2.21725 3.30671 2.67822 2.84573C3.1392 2.38476 3.88782 2.38476 4.34879 
+          2.84573L4.34511 2.84942Z" fill="black"/>
+        </svg>
+      </div>
+      
     </div>
 
     <div  class="flex flex-row  lg:flex-wrap lg:justify-start  overflow-x-auto gap-4">
