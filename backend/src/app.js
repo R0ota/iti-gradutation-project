1f0bbdf796906006e76b21162fcf85717d9ce372
omar <omarelsayed.oes@gmail.com>
2025-03-29 13:29:19 +0200

    Add product management functionality with CRUD operations and update app routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const {authMiddleware,adminMiddleware}=require("./middleware/authentication")
 const connectDB = require('./config/connectDB');
 const auth = require('./routes/auth');
 const userRoute = require('./routes/userRoute');
+const productRoute = require('./routes/productRoutes');
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,6 +21,7 @@ app.use('/', auth);
 // setRoutes(app);
 app.use('/user', authMiddleware, userRoute);
 app.use('/admin', authMiddleware, adminMiddleware, userRoute);
+app.use('/products', productRoute);
 app.listen(PORT, async() => {
     await connectDB();
     
