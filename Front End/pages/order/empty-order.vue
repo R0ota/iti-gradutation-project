202a0ef4243a20c4f1d96fc6603ba3c785de79aa
Nourhan Ibrahim <norhanibrahim289@gmail.com>
2025-04-04 03:29:22 +0200

    order-empty page

---
+++ Front End/pages/order/empty-order.vue
@@ -0,0 +1,24 @@
+<script setup>
+definePageMeta({
+  layout: "order",
+});
+</script>
+
+<template>
+     <div class="w-full h-screen flex items-center justify-center ">
+        <div class="flex flex-col items-center justify-center gap-4">
+
+            <img src="~/assets/empty-order.png" class="lg:w-48 lg:h-48 w-32 h-32">
+
+            <div class="flex flex-col items-center justify-center gap-1">
+                <P class="text-black text-xl font-medium leading-loose">
+                    You have no orders yet.
+                </P>
+                <NuxtLink to="/market" class="cursor-pointer px-6 py-2 text-red-800 text-lg font-bold leading-relaxed">
+                    Go Shopping
+                </NuxtLink>
+            </div>
+            
+        </div>
+    </div>
+</template>
