76b38153a54dbd5f1f473ca1390288ed7e67c1b6
Nourhan Ibrahim <norhanibrahim289@gmail.com>
2025-04-04 05:35:42 +0200

    ongoing order

---
+++ Front End/components/order/order-nvegation.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="border-b-2 border-red-800 gap-6 flex w-full">
+      <NuxtLink
+        tabindex="0"
+        class="px-4 py-2 flex justify-center items-center gap-2.5 cursor-pointer focus:bg-red-800 focus:rounded-tl-3xl focus:rounded-tr-3xl group"
+      >
+        <p
+          class="text-red-800 text-xl font-medium group-focus:font-bold group-focus:text-yellow-50"
+        >
+          Ongoing Orders
+        </p>
+      </NuxtLink>
+      <NuxtLink
+        tabindex="0"
+        class="px-4 py-2 flex justify-center items-center gap-2.5 cursor-pointer focus:bg-red-800 focus:rounded-tl-3xl focus:rounded-tr-3xl group"
+      >
+        <p
+          class="text-red-800 text-xl font-medium group-focus:font-bold group-focus:text-yellow-50"
+        >
+          Previous Orders
+        </p>
+      </NuxtLink>
+    </div>
+</template>
---
+++ Front End/pages/order/ongoing-order.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { NuxtLink } from "#components";
+
+definePageMeta({
+  layout: "order",
+});
+// const { data } = await useAsyncFetch("get", "/products");
+</script>
+
+<template>
+  <div class="ml-[120px] mr-[120px] pt-10 pb-20 flex flex-col gap-10">
+    <OrderNvegation/>
+    <div class="w-[533px] p-6 bg-orange-200 rounded-[48px] flex flex-col justify-center items-start gap-2.5">
+        <div class="h-44 inline-flex justify-start items-center gap-4 w-full">
+            <img src="" class="w-44 h-44 bg-yellow-50 rounded-[40px] "/>
+            <div class=" py-2 h-44 flex flex-col justify-between items-center w-full">
+                <div class="flex flex-col gap-2 jutify-start items-start w-full">
+                    <div class="flex justify-between items-center w-full">
+                        <p class="text-neutral-800 text-lg font-semibold font-['Poppins'] leading-relaxed">Order Status</p>
+                        <p class="text-neutral-800 text-lg font-semibold font-['Poppins'] leading-relaxed">Processing</p>
+                    </div>
+                    <div class="flex justify-between items-center w-full">
+                        <p class="text-neutral-800 text-lg font-semibold font-['Poppins'] leading-relaxed">Quantity</p>
+                        <p class="text-neutral-800 text-lg font-semibold font-['Poppins'] leading-relaxed">15 items</p>
+                    </div>
+                    <div class="flex justify-between items-center w-full">
+                        <p class="text-neutral-800 text-lg font-semibold font-['Poppins'] leading-relaxed">Order Date</p>
+                        <p class="text-neutral-800 text-lg font-semibold font-['Poppins'] leading-relaxed">25 / 01 / 2025</p>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center w-full">
+                    <p class="text-red-800 text-xl font-semibold font-['Poppins']">Total Price</p>
+                        <p class="text-red-800 text-xl font-semibold font-['Poppins']">900 EGP</p>
+                    </div>
+                
+            </div>
+        </div>
+        
+
+    </div>
+  </div>
+</template>
