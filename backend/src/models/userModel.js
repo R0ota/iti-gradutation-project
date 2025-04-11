0e90102d931368fb940d9ab8f61e00f63e4659cc
reham muhammed <reham.mohamed.ma@gmail.com>
2025-04-11 18:46:40 +0200

    Add username field to user model and update signup route

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
+  username:  String , 
+
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "admin"], required: true,default:"user" },
