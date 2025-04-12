6a09844c8ea2d9b5edbcba0d31c77785be221196
reham muhammed <reham.mohamed.ma@gmail.com>
2025-04-12 20:20:55 +0200

    Comment out useAsyncFetch function in useAsyncFetch.js

--- Front End/composables/useAsyncFetch.js
+++ Front End/composables/useAsyncFetch.js
@@ -1,7 +1,7 @@
-export const useAsyncFetch = (method, endpoint) => {
-    const config = useRuntimeConfig();
-    return useFetch (endpoint, {
-        method : method,
-        baseURL: "https://dummyjson.com/",
-    })
-}
+// export const useAsyncFetch = (method, endpoint) => {
+//     const config = useRuntimeConfig();
+//     return useFetch (endpoint, {
+//         method : method,
+//         baseURL: "https://dummyjson.com/",
+//     })
+// }
