fe9b1d2a13c9d76faf52570102ad6124fdfb9692
omar <omarelsayed.oes@gmail.com>
2025-04-15 19:18:44 +0200

    update upload-product-data.vue design

--- Front End/pages/admin/product/upload-product-data.vue
+++ Front End/pages/admin/product/upload-product-data.vue
@@ -26,7 +26,7 @@ const labelCasses = "px-2 text-red-900 text-xl font-[Poppins] font-medium";
 </script>
 
 <template>
-    <div class="flex justify-between items-center m-[30px] ml-[250px] fixed w-[77%]">
+    <div class="flex justify-between items-center m-[30px]  w-[77%]">
         <!-- form data -->
         <form class="flex flex-row items-start justify-start flex-1 gap-8">
             <div class="flex flex-col gap-4 items-start justify-start w-full">
@@ -46,6 +46,19 @@ const labelCasses = "px-2 text-red-900 text-xl font-[Poppins] font-medium";
                         class="text-yellow-50 text-lg font-semibold font-['Poppins'] cursor-pointer px-4 py-1.5 bg-red-900 rounded-xl shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)]">
                         Upload
                     </button>
+
+                </div>
+                
+                <div class="flex items-center justify-between gap-0.5 w-full">
+                    <div class="relative inline-block group">
+                                    <!-- The text -->
+                                    <span class="text-black cursor-pointer text-base font-medium font-['Poppins'] leading-normal">Image Link</span>
+
+                                    <!-- The image (hidden by default, shown on hover) -->
+                                    <img src="https://placehold.co/600x400/orange/white" alt="Preview"
+                                        class="absolute left-0 mt-2 w-40 h-auto rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
+                                </div>
+                                <img src="../../../assets/remove.svg" alt="" class="cursor-pointer" onclick="this.parentNode.remove();">
 
                 </div>
                 <!-- category -->
@@ -77,26 +90,46 @@ const labelCasses = "px-2 text-red-900 text-xl font-[Poppins] font-medium";
                         <option value="Design Category">Design Category</option>
                     </select>
                 </div>
-
-            </div>
-
-            <div class="flex flex-col gap-3 w-full">
-                <!-- price -->
-                <AdminInputData label="Price" placeholder="100.00 EGP" v-model="price" />
+                 <!-- price -->
+                 <AdminInputData label="Price" placeholder="100.00 EGP" v-model="price" />
                 <!-- <h2 class="text-black font-bold text-xl">Price Range</h2> -->
             <!-- design price range -->
+             <label for="Price Range" class="font-semibold font-['Poppins'] leading-relaxed">Design Price Range</label>
                 <div class="flex flex-row  gap-1 w-full">
                     <AdminInputData label="Min Price" placeholder="100.00 EGP" v-model="minPrice" />
 
                     <AdminInputData label="Max Price" placeholder="300.00 EGP" v-model="maxPrice" />
                 </div>
+
+                <!-- action btns -->
+                <div class="flex justify-end gap-4 w-full">
+                    <button
+                        class="text-red-900 text-lg font-semibold font-['Poppins'] cursor-pointer px-4 py-1.5 bg-white rounded-xl shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] outline-[1.50px] outline-offset-[-1.50px] outline-red-900">
+                        Cancel
+                    </button>
+                    <button @click.prevent="saveDesign"
+                        class="text-yellow-50 text-lg font-semibold font-['Poppins'] cursor-pointer px-4 py-1.5 bg-red-900 rounded-xl shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)]">
+                        Save
+                    </button>
+                </div>
+
+            </div>
+
+            <div class="flex flex-col gap-3 w-full">
+               
                 <!-- size -->
                 <div class="flex flex-col gap-2 w-full">
-                    <label for="input" :class="labelCasses">Size</label>
+                    <div class="flex flex-row justify-between w-full">
+                        <label for="input" :class="labelCasses">Size</label>
+                        <div class="flex flex-row gap-2 items-center">
+                            <img src="../../../assets/CheckChoice.svg" class=" size-5" alt="">
+                            <label for="size" class="text-black text-base font-medium font-['Poppins'] leading-normal" >Choice</label>
+                        </div>
+                    </div>
+                   
                     <div id="size" class="flex flex-col gap-1 w-full justify-center">
-                        <div class="flex flex-row gap-0.5 w-full">
+                        <div class="flex flex-row gap-10 w-full">
                             <AdminInputData placeholder="e.g. S, M, L, XL" v-model="size" />
-                            <div class="w-full"></div>
                             <img src="../../../assets/minus-solid.svg" alt="" onclick="this.parentNode.remove();"/>
                            
 
@@ -108,14 +141,22 @@ const labelCasses = "px-2 text-red-900 text-xl font-[Poppins] font-medium";
                 </div>
                 <!-- color -->
                 <div class="flex flex-col gap-0.5 w-full">
-                    <label for="input" :class="labelCasses">Color</label>
+                   
+                    <div class="flex flex-row justify-between w-full">
+                        <label for="input" :class="labelCasses">Color</label>
+                        <div class="flex flex-row gap-2 items-center">
+                            <img src="../../../assets/image.svg" class=" size-5" alt="">
+                            <label for="size" class="text-black text-base font-medium font-['Poppins'] leading-normal" >Image</label>
+                        </div>
+                    </div>
+                    
                     <div id="color" class="flex flex-col gap-2 w-full justify-center">
-                        <div class="flex flex-row gap-2 w-full items-center">
+                        <div class="flex flex-row w-full items-center gap-5">
                             <AdminInputData placeholder="e.g. Red, Blue, Green" v-model="color" />
-                            <span class="text-black w-full text-center">
+                            <span class="text-black w-full text-right">
                                 <div class="relative inline-block group">
                                     <!-- The text -->
-                                    <span class="text-blue-600 cursor-pointer">Image Link</span>
+                                    <span class="text-black cursor-pointer">Image Link</span>
 
                                     <!-- The image (hidden by default, shown on hover) -->
                                     <img src="https://placehold.co/600x400/orange/white" alt="Preview"
@@ -139,17 +180,7 @@ const labelCasses = "px-2 text-red-900 text-xl font-[Poppins] font-medium";
     class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
     multiple
   > -->
-                <!-- action btns -->
-                <div class="flex justify-end gap-4 w-full">
-                    <button
-                        class="text-red-900 text-lg font-semibold font-['Poppins'] cursor-pointer px-4 py-1.5 bg-white rounded-xl shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] outline-[1.50px] outline-offset-[-1.50px] outline-red-900">
-                        Cancel
-                    </button>
-                    <button @click.prevent="saveDesign"
-                        class="text-yellow-50 text-lg font-semibold font-['Poppins'] cursor-pointer px-4 py-1.5 bg-red-900 rounded-xl shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)]">
-                        Save
-                    </button>
-                </div>
+                
 
             </div>
         </form>
